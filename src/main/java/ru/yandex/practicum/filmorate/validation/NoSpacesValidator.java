636451c9b8bede0b521b30272f7f05fcbe96bff4
EvgeniyBushuev <johnyk359@yandex.ru>
2023-04-22 23:34:48 +0300

    Перезалив ветки

---
+++ src/main/java/ru/yandex/practicum/filmorate/validation/NoSpacesValidator.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class NoSpacesValidator implements ConstraintValidator<NoSpace, String> {
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return !s.contains(" ");
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmValidationTest.java
@@ -0,0 +1,64 @@
+package ru.yandex.practicum.filmorate.model;
+
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.ValidationException;
+import javax.validation.Validator;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilmValidationTest {
+    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    private Film getNewFilm() {
+        Film film = new Film();
+        film.setName("Зеленая Миля");
+        film.setDescription("В тюрьме для смертников появляется заключенный с божественным даром. Мистическая драма по роману Стивена Кинга");
+        film.setReleaseDate(LocalDate.of(1999, 12, 6));
+        film.setDuration(189);
+        return film;
+    }
+
+    @Test
+    void shouldPassValidationWithValidFilm() {
+        Film film = getNewFilm();
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(0, violations.size());
+    }
+
+    @Test
+    void shouldNotPassValidationIfFilmTitleIsBlank() {
+        Film film = getNewFilm();
+        film.setName("");
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1, violations.size());
+    }
+
+    @Test
+    void shouldNotPassValidationIfDescriptionIsMoreThan200Symbols() {
+        Film film = getNewFilm();
+
+        film.setDescription("!".repeat(201));
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1, violations.size());
+
+    }
+
+    @Test
+    void shouldNotPassValidationIfDateIsLessThanFilmIndustryStarted() {
+        Film film = getNewFilm();
+
+        film.setReleaseDate(LocalDate.of(1895, 11, 28));
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertEquals(1, violations.size());
+
+    }
+}
