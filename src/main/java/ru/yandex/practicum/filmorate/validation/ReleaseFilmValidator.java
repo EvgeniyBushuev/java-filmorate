c9dd7ac17137156c335606c32856f558dc26d68a
EvgeniyBushuev <johnyk359@yandex.ru>
2023-04-24 00:29:17 +0300

    CodeStyle

--- src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseFilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseFilmValidator.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 public class ReleaseFilmValidator implements ConstraintValidator<FilmRelease, LocalDate> {
 
     private final LocalDate FILM_INDUSTRY_START = LocalDate.of(1895, 12, 28);
+
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
         return (localDate.isAfter(FILM_INDUSTRY_START));
