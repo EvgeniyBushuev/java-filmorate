e495f8b2c81a627b4f5d190c38e540e476f6eaa9
EvgeniyBushuev <johnyk359@yandex.ru>
2023-05-06 16:46:33 +0300

    CodeStyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,14 +2,12 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -55,7 +53,7 @@ public class FilmService {
         film.getLikes().remove(user.getId());
     }
 
-    public List<Film> getTopFilms (long count) {
+    public List<Film> getTopFilms(long count) {
         return filmStorage.getAll()
                 .stream()
                 .sorted((f1, f2) -> f2.getLikes().size() - f1.getLikes().size())
