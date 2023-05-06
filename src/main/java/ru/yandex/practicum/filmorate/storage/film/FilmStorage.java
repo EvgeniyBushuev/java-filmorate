66cdbc74f111dbdb339240fb25b53ca345f78255
EvgeniyBushuev <johnyk359@yandex.ru>
2023-05-06 16:55:06 +0300

    CodeStyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -4,9 +4,14 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film add(Film film);
+
     long delete(long id);
+
     Film update(Film film);
+
     Film get(long id);
+
     List<Film> getAll();
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -4,9 +4,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User add(User user);
+
     long delete(long id);
+
     User update(User user);
+
     User get(long id);
+
     List<User> getAll();
 }
